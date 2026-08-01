e51bf0d193e833b6d86c14c61a09fab6aa1b56d6
Soham Dube <ssohamdube@gmail.com>
2026-08-01 17:59:23 +0530

    fix: remove conflicting approval_prompt from Google OAuth config

--- lib/auth.ts
+++ lib/auth.ts
@@ -14,7 +14,6 @@ export const authOptions: NextAuthOptions = {
           prompt: "consent",
           access_type: "offline",
           response_type: "code",
-          approval_prompt: "force",
           scope:
             "openid email profile " +
             "https://www.googleapis.com/auth/gmail.readonly " +
